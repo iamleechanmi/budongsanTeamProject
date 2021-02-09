0b8b5588b17a72b5fa50dca2baf7cdb13eb10e82
iamleechanmi <leechanmi@kakao.com>
2021-02-09 21:12:57 +0900

    Modify query

--- [05]DML/업체.sql
+++ [05]DML/업체.sql
@@ -1,6 +1,7 @@
 -- 업체.sql
-
+-------------------------------------------------------------------------------
 -- 회원 가입
+-------------------------------------------------------------------------------
 insert into tblFirm values (seqFirm.nextVal,
                             '아이디', 
                             '비밀번호', 
@@ -13,95 +14,276 @@ insert into tblFirm values (seqFirm.nextVal,
                             0,
                             0, 
                             (select seq from tblCategory where categoryNum = '카테고리명(청소/시공)'));
-
+-------------------------------------------------------------------------------
 -- 아이디 찾기 
+-------------------------------------------------------------------------------
 select email from tblFirm where id = '업체명' and tel = '연락처';
-
+-------------------------------------------------------------------------------
 -- 비밀번호 찾기
+-------------------------------------------------------------------------------
 select pw from tblFirm where email = '이메일' and tel = '연락처';
-
+-------------------------------------------------------------------------------
 -- 승인 업체 관련 뷰
+-------------------------------------------------------------------------------
 create or replace view vwApprovalF
 as
 select
     f.seq as seq, -- 업체 번호
     af.seq as aseq, -- 승인 업체 번호
-    c.categoryNum as category, -- 업체 카테고리
-    f.address as address, -- 업체 주소
-    f.tel as tel, -- 업체 연락처
-    f.introduction as introduction, -- 업체 소개글
-    f.portfolio as portfolio, -- 업체 포트폴리오
-    f.availablePoint as availablePoint, -- 보유 포인트
-    ul.seq as ulSeq, -- 포인트 사용 기록 번호
-    ul.useAmount as useAmount, -- 포인트 사용액
-    pl.seq as plSeq, -- 포인트 결제 기록 번호
-    pl.payment as payment, -- 포인트 결제액
-    af.approvalDate as approvalDate -- 승인 업체 승인 일자
+    f.id as id, -- 업체명(아이디)
+    c.categoryNum as categoryNum, -- 카테고리
+    f.email as email, -- 이메일
+    f.address as address, -- 주소
+    f.tel as tel, -- 연락처
+    f.introduction as introduction, -- 소개글
+    f.portfolio as portfolio -- 포트폴리오
 from tblFirm f
     inner join tblCategory c
         on c.seq = f.categorySeq
             inner join tblApprovalF af
-                on f.seq = af.firmSeq
-                    inner join tblUsageLog ul
-                        on af.seq = ul.approvalFSeq
-                            inner join tblPaymentLog pl
-                                on af.seq = pl.approvalFseq
-                                    where f.delFlag = 0
-                                        order by af.seq asc;
-                                        
-select * from vwApprovalF;
-
+                on af.firmSeq = f.seq
+                    where f.delFlag = 0
+                        order by f.seq desc;
+                                                
+select * from vwApprovalF;     
+-------------------------------------------------------------------------------
+-- 전문 업체 전체 목록 조회
+-- 최근에 등록된 업체 순
+-------------------------------------------------------------------------------
+select seq, id, categoryNum, email, address, tel, introduction, portfolio
+from (select af.*, rownum as rnum from vwApprovalF af)
+    where rnum between 10*(페이지수-1)+1 and 페이지수*10;
+-------------------------------------------------------------------------------    
+-- 특정 업체 조회
+-------------------------------------------------------------------------------
+select seq, id, categoryNum, email, address, tel, introduction, portfolio
+from (select af.*, rownum as rnum from vwApprovalF af where categoryNum like '%카테고리명(시공/청소)%')
+    where rnum between 10*(페이지수-1)+1 and 페이지수*10;
+-------------------------------------------------------------------------------
+-- 업체 견적서 등록 시 포인트 차감 트리거
+-- 하루 3번 무료, 이후 등록 시 500포인트 차감
+-------------------------------------------------------------------------------
+create or replace trigger trgUsePoint
+    before
+    insert on tblEstimate1th
+    for each row
+declare
+    vfirmSeq number;
+    vcnt number := 0; -- 오늘 몇번 등록했는지 셀 카운트 변수
+begin
+    select firmSeq into vfirmSeq from tblApprovalF where seq = :new.approvalFSeq;
+    
+    -- 업체 견적서 테이블에서 오늘 몇번 등록했는지 세서 vcnt에 저장
+    select nvl(max(count(approvalFSeq)), 0) into vcnt from tblEstimate1th 
+        where approvalFSeq = :new.approvalFSeq and to_char(regDate, 'yyyymmdd') = to_char(sysdate, 'yyyymmdd') 
+            group by approvalFSeq;
+        
+    -- 3번 이상일 경우 포인트 차감
+    if vcnt >= 3 then
+        -- 포인트 사용 기록 테이블에 추가
+        insert into tblUsageLog values (seqUsageLog.nextVal, default, 500, :new.approvalFSeq);
+    
+        -- 업체 테이블의 보유 포인트 수정
+        update tblFirm set availablePoint = availablePoint - 500 where seq = vfirmSeq;
+    end if;
+end;
+-------------------------------------------------------------------------------
+-- 업체 견적서 작성
+-------------------------------------------------------------------------------
+create or replace procedure procAddEstimate1th(
+    paseq number, -- 승인 업체 번호
+    prseq number, -- 고객 요청서 번호
+    pcost number, -- 예상 비용
+    pcontent varchar2 -- 견적서 내용
+)
+is
+begin
+    insert into tblEstimate1th 
+        values (seqEstimate1th.nextVal, default, paseq, prseq, pcost, pcontent, 0);
+    commit;
+exception
+    when others then
+        rollback;
+end procAddEstimate1th;
+-------------------------------------------------------------------------------
 -- 포인트 충전
--- 결제와 사용 로그 테이블은 있는데 결제 받는 곳이 어디인지 보고 작성
-
--- 업체 견적서 관련 뷰
+-------------------------------------------------------------------------------
+create or replace procedure procAddPaymentLog(
+    paseq number, -- 승인 업체 번호
+    ppayment number -- 결제액
+)
+is
+begin
+    -- 포인트 결제 기록 테이블에 추가
+    insert into tblPaymentLog 
+        values (seqPaymentLog.nextVal, default, ppayment, paseq);
+    
+    -- 업체 테이블의 보유 포인트 추가
+    update tblFirm set availablePoint = availablePoint + ppayment
+        where seq = (select firmSeq from tblApprovalF where seq = paseq);
+    commit;
+exception
+    when others then
+        rollback;
+end procAddPaymentLog;
+-------------------------------------------------------------------------------
+-- 포인트 충전 내역 조회 (길어지면 where절 추가)
+-------------------------------------------------------------------------------
+select
+    seq, -- 포인트 결제 기록 번호
+    payment, -- 포인트 결제액
+    paymentDate -- 포인트 결제 일자
+from (select seq, payment || 'p' as payment, paymentDate, approvalFSeq, rownum as rnum 
+        from tblPaymentLog where approvalFSeq = 승인업체번호);
+-------------------------------------------------------------------------------
+-- 포인트 사용 내역 조회
+-------------------------------------------------------------------------------
+select 
+    seq, -- 포인트 사용 기록 번호
+    useAmount, -- 포인트 사용액
+    usageDate -- 포인트 사용 일자
+from (select seq, useAmount, usageDate, approvalFSeq, rownum as rnum 
+        from tblUsageLog where approvalFSeq = 승인업체번호);
+--    where rnum between 2 and 5;
+-------------------------------------------------------------------------------    
+-- 특정 업체 견적서 조회
+-------------------------------------------------------------------------------
+select
+    e1.seq as seq, -- 업체 견적서
+    va.id as id, -- 업체 아이디(업체명)
+    e1.estimatedCost as estimatedCost, -- 견적 예상 비용
+    e1.econtent as econtent, -- 견적서 내용
+    e1.regDate as regDate, -- 작성일
+    decode(e1.adoptFlag, 1, '채택', 0, '미채택') as adoptFlag -- 채택 여부
+from vwApprovalF va
+    inner join tblEstimate1th e1
+        on va.aseq = e1.approvalFSeq
+            where va.aseq = 승인업체번호
+                order by regDate desc;
+-------------------------------------------------------------------------------
+-- 업체가 보낸 견적서를 고객이 조회
+-- 가장 최근에 받은 견적서가 제일 상단에
+-------------------------------------------------------------------------------
+create or replace procedure procFindEstimate1th(
+    prequestSeq number, -- 고객 요청서 번호
+    pcursor out sys_refcursor
+)
+is
+begin
+    open pcursor for select
+                            seq, -- 업체 견적서 번호
+                            (select id from vwApprovalF where aseq = approvalFSeq) as id, -- 업체명(아이디)
+                            (select portfolio from vwApprovalF where aseq = approvalFSeq) as portfolio, -- 포트폴리오
+                            estimatedCost, -- 예상 비용
+                            econtent, -- 견적서 내용
+                            regDate -- 견적서 작성일
+                    from tblEstimate1th
+                        where requestSeq = prequestSeq
+                            order by seq desc;
+end procFindEstimate1th;
+-------------------------------------------------------------------------------
+-- 고객 요청서 작성
+-------------------------------------------------------------------------------
+create or replace procedure procAddRequest(
+    pseq number, -- 회원 번호
+    paddress varchar2, -- 주소
+    pshape varchar2, -- 가옥 구조
+    pdesiredDay date, -- 서비스 시작일
+    pdetail varchar2, -- 상세 전달 사항
+    parea number, -- 면적
+    pstype varchar2 -- 서비스 품목
+)
+is
+begin
+    insert into tblRequest 
+        values (seqRequest.nextVal, default, paddress, pshape, pdesiredDay, pdetail, parea, (select seq from tblService where stype = pstype), pseq);
+    commit;
+exception
+    when others then
+        rollback;
+end procAddRequest;
+-------------------------------------------------------------------------------
+-- 1:1 온라인 상담 내역 조회 - 특정 견적서에 한해
+-------------------------------------------------------------------------------
+select 
+    decode(whoFlag, 1, '업체', 0, '회원') as whoFlag, -- 작성자
+    chatContent, -- 글 내용
+    to_char(timeSent, 'hh24:mi') as timeSent -- 발송 시간
+from tblChatAsk
+    where estimate1thSeq = 업체견적서번호;
+-------------------------------------------------------------------------------
+-- 업체 견적서를 바탕으로 한 회원과 업체 관련 뷰
+-------------------------------------------------------------------------------
 create or replace view vwEstimate1th
 as
-select 
-    f.seq as seq, -- 업체 번호
-    af.seq as aseq, -- 승인 업체 번호
+select
+    af.aseq as aseq, -- 승인 업체 번호
+    af.id as id, -- 업체 아이디(업체명)
     e1.seq as eseq, -- 업체 견적서 번호
-    r.seq as rseq, -- 고객 요청서 번호
-    m.seq as mseq, -- 고객(회원) 번호
-    s.stype as type, -- 서비스 품목(청소/시공)
-    r.address as address, -- 주소지
-    r.shape as shape, -- 가옥 구조
-    r.desiredDay as desiredDay, -- 서비스 시작일
-    r.detail as detail, -- 상세 전달사항
-    r.area as area, -- 면적
-    r.regDate as rregDate, -- 고객요청서 작성일
-    e1.estimatedCost as estimatedCost, -- 예상 비용
-    e1.econtent as econtent, -- 견적서 내용
-    e1.adoptFlag as adoptFlag, -- 채택 여부
-    e1.regDate as eregDate -- 업체 견적서 작성일
-from tblEstimate1th e1
-    inner join tblApprovalF af
-        on e1.approvalFSeq = af.seq
-            inner join tblFirm f
-                on f.seq = af.firmSeq
-                    inner join tblRequest r
-                        on r.seq = e1.requestSeq
-                            inner join tblService s
-                                on s.seq = r.serviceSeq
-                                    inner join tblMember m
-                                        on m.seq = r.memberSeq
-                                            where m.delFlag = 0 and f.delFlag = 0
-                                                order by e1.seq;
+    m.seq as mseq, -- 회원 번호
+    r.seq as rseq -- 고객 요청서 번호
+from vwApprovalF af
+    inner join tblEstimate1th e1
+        on af.seq = e1.approvalFSeq
+            inner join tblRequest r
+                on r.seq = e1.requestSeq
+                    inner join tblService s
+                        on s.seq = r.serviceSeq
+                            inner join tblMember m
+                                on m.seq = r.memberSeq
+                                    order by e1.regDate desc;
 
-select * from vwestimate1th;
-    
-    
+select * from vwEstimate1th;
+-------------------------------------------------------------------------------
+-- 1:1 온라인 상담 내역 조회 - 업체별 목록 조회
+-------------------------------------------------------------------------------
+select
+    distinct ve.id as id -- 업체 아이디(업체명)
+from vwEstimate1th ve
+    left outer join tblChatAsk ca
+        on ve.eseq = ca.estimate1thSeq
+            where ve.mseq = 회원번호(18);
+-------------------------------------------------------------------------------
+-- 1:1 온라인 상담 - 회원이 메시지 작성
+-------------------------------------------------------------------------------
+create or replace procedure procMemberChatAsk(
+    peseq number, -- 업체 견적서 번호
+    pchatContent varchar2 -- 글 내용    
+)
+is
+begin
+    insert into tblChatAsk values (seqChatAsk.nextVal, default, peseq, pchatContent, 0);
+    commit;
+exception
+    when others then
+        rollback;
+end procMemberChatAsk;
+-------------------------------------------------------------------------------
+-- 1:1 온라인 상담 - 업체가 메시지 작성
+------------- 위랑 같은 프로시저인데 일단... 이걸로 하고 보류...
+-------------------------------------------------------------------------------
+create or replace procedure procFirmChatAsk(
+    peseq number, -- 업체 견적서 번호
+    pchatContent varchar2 -- 글 내용    
+)
+is
+begin
+    insert into tblChatAsk values (seqChatAsk.nextVal, default, peseq, pchatContent, 1);
+    commit;
+exception
+    when others then
+        rollback;
+end procFirmChatAsk;
 
-
-select * from tblApprovalF;
-select * from tblFirm;
-
-select * from tblCategory;
-
-select * from tblUsageLog;
-
-select * from tblPaymentLog;
-
+-------------------------------------------------------------------------------
 -- drop
+-------------------------------------------------------------------------------
 drop view vwApprovalF;
+drop trigger trgUsePoint;
+drop procedure procAddEstimate1th;
+drop procedure procAddPaymentLog;
+drop procedure procFindEstimate1th;
+drop procedure procAddRequest;
 drop view vwEstimate1th;
+drop procedure procMemberChatAsk;
+drop procedure procFirmChatAsk;
--- [05]DML/회원.sql
+++ [05]DML/회원.sql
@@ -1,17 +1,21 @@
 -- 회원 dml
-
+-------------------------------------------------------------------------------
 -- 회원 가입
+-------------------------------------------------------------------------------
 insert into tblMember values (seqMember.nextVal, '이메일주소', '비밀번호', '이름', '주민등록번호', '핸드폰번호', '주소', 0);
-
+-------------------------------------------------------------------------------
 -- 아이디 찾기
+-------------------------------------------------------------------------------
 select email from tblMember where name = '이름' and ssn = '주민등록번호' and phone = '핸드폰번호';
-    
+-------------------------------------------------------------------------------    
 -- 비밀번호 찾기
+-------------------------------------------------------------------------------
 select pw from tblMember where name = '이름' and ssn = '주민등록번호' and email = '이메일주소';
-
+-------------------------------------------------------------------------------
 -- 방 게시글 관련 뷰
 -- 방 게시글 관련해서는 이 뷰만 사용해서 가져오려고 생성했습니다.
 -- 더 필요한 컬럼 있으면 여기에 추가하면 될 것 같습니다.
+-------------------------------------------------------------------------------
 create or replace view vwRoomPost
 as
 select
@@ -53,24 +57,35 @@ from tblMember m
                                             inner join tblRoomBasic rb
                                                 on rb.seq = ri.roomBasicSeq
                                                     where rpost.delFlag = 0
-                                                        order by rpost.seq;
+                                                        order by rpost.regDate desc;
                                                                 
 select * from vwRoomPost;
-
+-------------------------------------------------------------------------------
 -- 방 게시글 조회
+-- 화면 설계보고 와일드카드에서 컬럼명으로 수정해놓겠습니다.
+-------------------------------------------------------------------------------
+-- 특정 게시글 조회 시
+select * from vwRoomPost where seq = 방게시글번호;
+
 -- 주소 조회 시
-select * from vwRoomPost where address like '%검색어%';
-
+select * from (select rownum as rnum, rp.* from vwRoomPost rp where address like '%검색어%')
+    where rnum between 10*(페이지수-1)+1 and 페이지수*10;
+    
 -- 제목 조회 시
-select * from vwRoomPost where subject like '%검색어%';
-
+select * from (select rownum as rnum, rp.* from vwRoomPost rp where subject like '%검색어%')
+    where rnum between 10*(페이지수-1)+1 and 페이지수*10;
+    
 -- 건물 유형 조회 시
-select * from vwRoomPost where btype like '%검색어%';
-
+select * from (select rownum as rnum, rp.* from vwRoomPost rp where btype like '%검색어%')
+    where rnum between 10*(페이지수-1)+1 and 페이지수*10;
+    
 -- 거래 유형 조회 시
-select * from vwRoomPost where dtype like '%검색어%';
-
+select * from (select rownum as rnum, rp.* from vwRoomPost rp where dtype like '%검색어%')
+    where rnum between 10*(페이지수-1)+1 and 페이지수*10;
+-------------------------------------------------------------------------------
 -- 방 게시글 작성
+-- 작성 시 이미지 추가가 필수가 아니라면... 아닌 게 좋을 것 같기도 하고... 정해지면 수정 예정
+-------------------------------------------------------------------------------
 create or replace procedure procAddRoomPost(
     pmSeq number, -- 회원 번호
     psubject varchar2, -- 글 제목
@@ -124,9 +139,9 @@ end procAddRoomPost;
 --begin
 --    procAddRoomPost(1, '방 내놓습니다~', '방 중개해주실 중개사분 연락주세요~', '오피스텔', '전세', '서울시 강남구 역삼동', 32.3, 35.5, 2, 6, 1, 1, '남', to_date('2012', 'yyyy'), 1, 1, 1, 1000, 0, 10, 'room0010.jpg');
 --end;
-
+-------------------------------------------------------------------------------
 -- 방 게시글 수정
--- <*미완료> 공란일 경우 처리하기
+-------------------------------------------------------------------------------
 create or replace procedure procReplaceRoomPost(
     pseq number, -- 게시글 번호
     psubject varchar2, -- 글 제목
@@ -195,34 +210,53 @@ end procReplaceRoomPost;
 --end;
                                                                 
 --select * from vwRoomPost where seq = 46;
-
+-------------------------------------------------------------------------------
 -- 방 게시글 삭제
+-------------------------------------------------------------------------------
 update tblRoomPost set delFlag = 1 where seq = 게시글번호;
-
+-------------------------------------------------------------------------------
 -- 방 이미지 추가
-insert into tblRoomImg values (seqRoomImg.nextVal, '추가할 주소', 방 게시글 번호);
-
+-------------------------------------------------------------------------------
+insert into tblRoomImg values (seqRoomImg.nextVal, '추가할 이미지 주소', 방 게시글 번호);
+-------------------------------------------------------------------------------
 -- 방 이미지 수정
+-------------------------------------------------------------------------------
 update tblRoomImg set url = '수정할 주소' where seq = 방 이미지 번호;
-
+-------------------------------------------------------------------------------
 -- 방 이미지 삭제
+-------------------------------------------------------------------------------
 delete from tblRoomImg where seq = 방 이미지 번호;
-
+-------------------------------------------------------------------------------
 -- 매물 게시글 관련 뷰
+-------------------------------------------------------------------------------
 create or replace view vwHousePost
 as
 select
     distinct hpost.seq as seq, -- 글 번호
     hpost.approBrokerSeq as bseq, -- 승인중개사 번호
-    b.businessName as bname, -- 상호
+    b.businessName as bname, -- 사업자 상호
     b.tel as tel, -- 전화번호
+    decode(ds.state, 0, '가능', 1, '완료') as state, -- 거래 상태
     bt.type as btype, -- 건물 유형
     dt.type as dtype, -- 거래 유형
     hprice.price as price, -- 매물 가격
     hprice.rent as rent, -- 월세
     hprice.monthlyFee as monthlyFee, -- 월관리비
     hb.address as address, -- 주소
-    
+    hb.exclusiveArea as exclusiveArea, -- 전용 면적
+    hb.supplyArea as supplyArea, -- 공급 면적
+    hb.selectedFloor as selectedFloor, -- 해당 층
+    hb.totalFloor as totalFloor, -- 총 층
+    hb.roomNum as roomNum, -- 방 개수
+    hb.bathroomNum as bathroomNum, -- 욕실 개수
+    hb.direction as direction, -- 방향
+    to_char(hb.completionYear, 'yyyy') as completionYear, -- 준공년도
+    decode(hb.parkingFlag, 0, '불가능', 1, '가능') as parkingFlag, -- 주차 가능 여부
+    decode(hb.elevator, 0, '없음', 1, '있음') as elevator, -- 엘리베이터 유무
+    decode(hb.pet, 0, '불가능', 1, '가능') as pet, -- 반려동물 유무
+    hpost.subject as subject, -- 글 제목
+    hpost.content as content, -- 글 설명
+    hpost.regDate as regDate -- 등록일
 from tblBroker b
     inner join tblApproBroker ab
         on b.seq = ab.brokerSeq
@@ -238,13 +272,78 @@ from tblBroker b
                                                 on dt.seq = hi.houseDealTypeSeq
                                                     inner join tblHouseBasic hb
                                                         on hb.seq = hi.houseBasicSeq
-                                                            where b.delFlag = 0 and hpost.delFlag = 0
-                                                                order by hpost.seq;
-                                            
-                                
+                                                            inner join tblHouseDealState ds
+                                                                on ds.housePostSeq = hpost.seq
+                                                                    where b.delFlag = 0 and hpost.delFlag = 0
+                                                                        order by hpost.regDate desc;
+                                                                
+select * from vwHousePost;
+-------------------------------------------------------------------------------                                            
+-- 매물 게시글 조회
+-------------------------------------------------------------------------------                                            
+-- 특정 게시글 조회 시
+select * from vwHousePost where seq = 매물게시글번호;
 
+-- 주소 조회 시
+select * from (select rownum as rnum, hp.* from vwHousePost hp where address like '%검색어%')
+    where rnum between 10*(페이지수-1)+1 and 페이지수*10;
+    
+-- 제목 조회 시
+select * from (select rownum as rnum, hp.* from vwHousePost hp where subject like '%검색어%')
+    where rnum between 10*(페이지수-1)+1 and 페이지수*10;
+    
+-- 건물 유형 조회 시
+select * from (select rownum as rnum, hp.* from vwHousePost hp where btype like '%검색어%')
+    where rnum between 10*(페이지수-1)+1 and 페이지수*10;
+    
+-- 거래 유형 조회 시
+select * from (select rownum as rnum, hp.* from vwHousePost hp where dtype like '%검색어%')
+    where rnum between 10*(페이지수-1)+1 and 페이지수*10;
+-------------------------------------------------------------------------------
+-- 매물 게시글 작성
+-------------------------------------------------------------------------------
+create or replace procedure procAddHousePost(
+    pmSeq number, -- 회원 번호
+    psubject varchar2, -- 글 제목
+    pcontent varchar2, -- 글 설명
+    pbType varchar2, -- 건물 유형
+    pdType varchar2, -- 거래 유형
+    paddress varchar2, -- 주소
+    peArea number, -- 전용 면적
+    psArea number, -- 공급 면적
+    psFloor number, -- 해당 층
+    ptFloor number, -- 총 층
+    proomNum number, -- 방 개수
+    pbathroomNum number, -- 욕실 개수
+    pdirection varchar2, -- 방향
+    pcYear date, -- 준공년도
+    pparking number, -- 주차 가능 여부
+    pelevator number, -- 엘리베이터 여부
+    ppet number, -- 반려동물 여부
+    pprice number, -- 가격
+    prent number, -- 월세
+    pmonthlyFee number, -- 월관리비
+    purl varchar2 -- 이미지 주소
+)
+is
+begin
 
+exception
+    when others then
+        rollback;
+end procAddHousePost;
+-------------------------------------------------------------------------------
+-- 매물 게시글 수정
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- 매물 게시글 삭제
+-------------------------------------------------------------------------------
+update tblHousePost set delFlag = 1 where seq = 1;
+-------------------------------------------------------------------------------
 -- drop
+-------------------------------------------------------------------------------
 drop view vwRoomPost;
 drop procedure procAddRoomPost;
 drop procedure procReplaceRoomPost;
+drop view vwHousePost;
