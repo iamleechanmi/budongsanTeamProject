0735a604fc5b45edbd4d3e38ac39a7c1f62b7e61
iamleechanmi <leechanmi@kakao.com>
2021-02-09 22:37:06 +0900

    Add query

--- [05]DML/회원.sql
+++ [05]DML/회원.sql
@@ -62,7 +62,8 @@ from tblMember m
 select * from vwRoomPost;
 -------------------------------------------------------------------------------
 -- 방 게시글 조회
--- 화면 설계보고 와일드카드에서 컬럼명으로 수정해놓겠습니다.
+-- 조건 검색 시..... 따로 따로 작성하기는 했지만 경우의 수가 너무 많아져서
+-- db에서 처리 안 하고 where절에 and로 연결 연결하여 조회하면 편할 것 같습니다.
 -------------------------------------------------------------------------------
 -- 특정 게시글 조회 시
 select * from vwRoomPost where seq = 방게시글번호;
@@ -121,7 +122,7 @@ begin
                 
     -- 방 정보 테이블에 추가
     insert into tblRoomInfo
-        values (seqRoomInfo.nextVal, seqRoomBasic.currVal, (select seq from tblRoomBuildingType where type=pbType), (select seq from tblRoomDealType where type=pdType));
+        values (seqRoomInfo.nextVal, seqRoomBasic.currVal, (select seq from tblRoomBuildingType where type = pbType), (select seq from tblRoomDealType where type = pdType));
     
     -- 방 게시글 테이블에 추가
     insert into tblRoomPost
@@ -143,7 +144,7 @@ end procAddRoomPost;
 -- 방 게시글 수정
 -------------------------------------------------------------------------------
 create or replace procedure procReplaceRoomPost(
-    pseq number, -- 게시글 번호
+    pseq number, -- 방 게시글 번호
     psubject varchar2, -- 글 제목
     pdetail varchar2, -- 글 설명
     pbType varchar2, -- 건물 유형
@@ -260,9 +261,9 @@ select
 from tblBroker b
     inner join tblApproBroker ab
         on b.seq = ab.brokerSeq
-            inner join tblHousePost hpost
+            right outer join tblHousePost hpost
                 on ab.seq = hpost.approBrokerSeq
-                    inner join tblHousePrice hprice
+                    left outer join tblHousePrice hprice
                         on hprice.seq = hpost.housePriceSeq
                             inner join tblHouseInfo hi
                                 on hi.seq = hpost.houseInfoSeq
@@ -280,6 +281,8 @@ from tblBroker b
 select * from vwHousePost;
 -------------------------------------------------------------------------------                                            
 -- 매물 게시글 조회
+-- 조건 검색 시..... 따로 따로 작성하기는 했지만 경우의 수가 너무 많아져서
+-- db에서 처리 안 하고 where절에 and로 연결 연결하여 조회하면 편할 것 같습니다.
 -------------------------------------------------------------------------------                                            
 -- 특정 게시글 조회 시
 select * from vwHousePost where seq = 매물게시글번호;
@@ -303,7 +306,7 @@ select * from (select rownum as rnum, hp.* from vwHousePost hp where dtype like 
 -- 매물 게시글 작성
 -------------------------------------------------------------------------------
 create or replace procedure procAddHousePost(
-    pmSeq number, -- 회원 번호
+    pbSeq number, -- 승인 중개사 번호
     psubject varchar2, -- 글 제목
     pcontent varchar2, -- 글 설명
     pbType varchar2, -- 건물 유형
@@ -327,19 +330,128 @@ create or replace procedure procAddHousePost(
 )
 is
 begin
+    -- 매물 기초 정보 테이블에 추가
+    insert into tblHouseBasic
+        values (seqHouseBasic.nextVal, paddress, peArea, psArea, psFloor, ptFloor, proomNum, pbathroomNum, pdirection, pcYear, pparking, pelevator, ppet);
+    
+    -- 매물 가격 테이블에 추가
+    insert into tblHousePrice
+        values (seqHousePrice.nextVal, pprice, prent, pmonthlyFee);
 
+    -- 매물 정보 테이블에 추가
+    insert into tblHouseInfo
+        values (seqHouseInfo.nextVal, seqHouseBasic.currVal, (select seq from tblHouseBuildingType where type = pbType), (select seq from tblHouseDealType where type = pdType));
+    
+    -- 매물 게시글 테이블에 추가
+    insert into tblHousePost
+        values (seqHousePost.nextVal, pbSeq, seqHouseInfo.currVal, seqHousePrice.currVal, psubject, pcontent, default, 0);
+    
+    -- 매물 거래 상태 테이블에 추가
+    insert into tblHouseDealState
+        values (seqHouseDealState.nextVal, seqHousePost.currVal, 0);
+        
+    -- 매물 이미지 테이블에 추가
+    insert into tblHouseImg
+        values (seqHouseImg.nextVal, purl, seqHousePost.currVal);
+    commit;
 exception
     when others then
         rollback;
 end procAddHousePost;
+
+--begin
+--    procAddHousePost(1, '역삼역 희귀 매물!', '선착순!', '원룸', '월세', '서울시 강남구 역삼동 역삼빌딩', 32.3, 35.5, 2, 6, 1, 1, '남', to_date('2012', 'yyyy'), 1, 1, 1, 100, 100, 1, 'house0010.jpg');
+--end;
 -------------------------------------------------------------------------------
 -- 매물 게시글 수정
 -------------------------------------------------------------------------------
+create or replace procedure procReplaceHousePost(
+    pseq number, -- 매물 게시글 번호
+    psubject varchar2, -- 글 제목
+    pcontent varchar2, -- 글 설명
+    pbType varchar2, -- 건물 유형
+    pdType varchar2, -- 거래 유형
+    paddress varchar2, -- 주소
+    peArea number, -- 전용 면적
+    psArea number, -- 공급 면적
+    psFloor number, -- 해당 층
+    ptFloor number, -- 총 층
+    proomNum number, -- 방 개수
+    pbathroomNum number, -- 욕실 개수
+    pdirection varchar2, -- 방향
+    pcYear date, -- 준공년도
+    pparking number, -- 주차 가능 여부
+    pelevator number, -- 엘리베이터 여부
+    ppet number, -- 반려동물 여부
+    pprice number, -- 가격
+    prent number, -- 월세
+    pmonthlyFee number -- 월관리비
+)
+is
+    vhouseInfoSeq number; -- 매물 정보 번호 저장할 변수
+begin
+    select houseInfoSeq into vhouseInfoSeq from tblHousePost where seq = pseq; -- 매물 정보 번호 저장
+    
+    -- 매물 기초 정보 테이블 수정
+    update tblHouseBasic set address = paddress, 
+                            exclusiveArea = peArea, 
+                            supplyArea = psArea, 
+                            selectedFloor = psFloor, 
+                            totalFloor = ptFloor, 
+                            roomNum = proomNum, 
+                            bathroomNum = pbathroomNum, 
+                            direction = pdirection, 
+                            completionYear = pcYear, 
+                            parkingFlag = pparking, 
+                            elevator = pelevator, 
+                            pet = ppet
+                            where seq = (select houseBasicSeq from tblHouseInfo where seq = vhouseInfoSeq);
+    
+    -- 매물 건물 유형, 매물 거래 유형 수정 시 매물 정보 테이블 수정
+    update tblHouseInfo set houseBuildingTypeSeq = (select seq from tblHouseBuildingType where type = pbType),
+                            houseDealTypeSeq = (select seq from tblHouseDealType where type = pdType)
+                            where seq = vhouseInfoSeq;
+    
+    -- 매물 가격 테이블 수정
+    update tblHousePrice set price = pprice,
+                            rent = prent,
+                            monthlyFee = pmonthlyFee
+                            where seq = (select housePriceSeq from tblHousePost where seq = pseq);
 
+    -- 매물 게시글 테이블 수정
+    update tblHousePost set subject = psubject,
+                            content = pcontent
+                            where seq = pseq;
+    commit;
+exception
+    when others then
+        rollback;
+end procReplaceHousePost;
+
+--begin
+--    procReplaceHousePost(62, '역삼역 희귀 아파트 전세 매물!', '24시간 연락 받습니다.', '아파트', '전세', '서울시 강남구 역삼동 역삼빌딩', 10.10, 12.12, 2, 5, 1, 2, '남서', to_date('2015', 'yyyy'), 1, 0, 0, 100, 100, 1);
+--end;    
 -------------------------------------------------------------------------------
 -- 매물 게시글 삭제
 -------------------------------------------------------------------------------
 update tblHousePost set delFlag = 1 where seq = 1;
+-------------------------------------------------------------------------------
+-- 매물 이미지 추가
+-------------------------------------------------------------------------------
+insert into tblHouseImg values (seqHouseImg.nextVal, '추가할 이미지 주소', 매물 게시글 번호);
+--select * from tblHouseImg where housePostSeq = 62; -- 103
+-------------------------------------------------------------------------------
+-- 매물 이미지 수정
+-------------------------------------------------------------------------------
+update tblHouseImg set url = '수정할 주소' where seq = 매물 이미지 번호;
+--update tblHouseImg set url = 'house0020.jpg' where seq = 103;
+--select * from tblHouseImg where housePostSeq = 62;
+-------------------------------------------------------------------------------
+-- 매물 이미지 삭제
+-------------------------------------------------------------------------------
+delete from tblHouseImg where seq = 매물 이미지 번호;
+--delete from tblHouseImg where seq = 103;
+-------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 -- drop
 -------------------------------------------------------------------------------
@@ -347,3 +459,5 @@ drop view vwRoomPost;
 drop procedure procAddRoomPost;
 drop procedure procReplaceRoomPost;
 drop view vwHousePost;
+drop procedure procAddHousePost;
+drop procedure procReplaceHousePost;
